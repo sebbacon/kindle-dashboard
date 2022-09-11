35e95de054baef6bb04291553e28d0a13dd87b80
Seb Bacon <seb.bacon@gmail.com>
2022-09-11 11:18:57 +0100

    Remove old cruft

--- dashboard/bin/run.py
+++ dashboard/bin/run.py
@@ -4,18 +4,8 @@
 from datetime import datetime, timezone
 import os
 from os.path import join
-from extract import get_google_scholar, get_gwent_data, get_tvmaze_data
 from cal import get_events
 
-
-scholar_url = "http://scholar.google.com/citations?user=4niBmJUAAAAJ&hl=en"
-gwent_url = "http://www.playgwent.com/en/profile/sepro"
-tvmaze_ids = [
-    6,  # The 100
-    79,  # The Goldbergs
-    38963,  # The Mandalorian
-    17128,  # This Is Us
-]
 
 svg_path = os.path.join(os.environ["PYTHONPATH"], "dashboard", "svg")
 
@@ -39,45 +29,6 @@ def fmt_date(date_input):
 
 def is_today(date_input, fmt="%Y-%m-%d"):
     return date_input == datetime.now().strftime(fmt)
-
-
-def do_original_think():
-    # Get Data
-    gs_data = get_google_scholar(scholar_url)
-    gwent_data = get_gwent_data(gwent_url)
-    tvmaze_data = get_tvmaze_data(tvmaze_ids)
-
-    # Combine into dict
-    svg_data = {
-        "GS_HINDEX": gs_data.get("h_index"),
-        "GS_CITATIONS": gs_data.get("citations"),
-        "GWENT_LADDER_RANK": gwent_data.get("ladder")
-        + (
-            " (Rank " + gwent_data.get("rank") + ")"
-            if "Pro" not in gwent_data.get("ladder")
-            else ""
-        ),
-        "GWENT_MMR": gwent_data.get("mmr"),
-        "GWENT_POSITION": gwent_data.get("position"),
-        "LASTUPDATE": "Last Update: " + datetime.now().strftime("%d/%m/%Y - %H:%M:%S"),
-    }
-
-    for i in range(3):
-        if i < len(tvmaze_data):
-            svg_data["TV_SHOW_%d" % (i + 1)] = tvmaze_data[i]["name"]
-            svg_data["TV_EPISODE_%d" % (i + 1)] = tvmaze_data[i]["episode_name"]
-            svg_data["TV_AIRDATE_%d" % (i + 1)] = (
-                "TODAY"
-                if is_today(tvmaze_data[i]["airdate"])
-                else fmt_date(tvmaze_data[i]["airdate"])
-            )
-        else:
-            svg_data["TV_SHOW_%d" % (i + 1)] = "No upcoming episodes found"
-            svg_data["TV_EPISODE_%d" % (i + 1)] = ""
-            svg_data["TV_AIRDATE_%d" % (i + 1)] = ""
-
-    # Load Data into SVG
-    create_svg(svg_data, join(svg_path, "template2.svg"), join(svg_path, "tmp.svg"))
 
 
 def render_calendar():
